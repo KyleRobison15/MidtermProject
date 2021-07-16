7354da7b248aa10f2015835b6689c3b6717bf357
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-07-16 13:44:35 -0700

    Added toString to User

--- JPABrewBuds/src/main/java/com/skilldistillery/brewbuds/entities/User.java
+++ JPABrewBuds/src/main/java/com/skilldistillery/brewbuds/entities/User.java
@@ -61,4 +61,9 @@ public class User {
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
 }
