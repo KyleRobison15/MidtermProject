59de5fdedc29ed2fae79d7573a259bfc54c458b7
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-07-16 12:42:16 -0700

    Create schema, stub user table

---
+++ DB/beerbudsdb.sql
@@ -0,0 +1,140 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema beer_budsdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `beer_budsdb` ;
+
+-- -----------------------------------------------------
+-- Schema beer_budsdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `beer_budsdb` DEFAULT CHARACTER SET utf8 ;
+USE `beer_budsdb` ;
+
+-- -----------------------------------------------------
+-- Table `beer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `beer` ;
+
+CREATE TABLE IF NOT EXISTS `beer` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `description` TEXT NULL,
+  `srm_value` INT NULL,
+  `alcohol_content` DOUBLE NULL,
+  `ibu` INT NULL,
+  `category_id` INT NOT NULL,
+  `brewery_id` INT NOT NULL,
+  `average_rating` DOUBLE NULL,
+  `rating_id` INT NULL,
+  `user_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `brewery`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `brewery` ;
+
+CREATE TABLE IF NOT EXISTS `brewery` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `description` TEXT NULL,
+  `address_id` INT NULL,
+  `average_rating` DOUBLE NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `address` VARCHAR(100) NULL,
+  `address2` VARCHAR(100) NULL,
+  `city` VARCHAR(100) NOT NULL,
+  `state_province` VARCHAR(100) NOT NULL,
+  `postal_code` VARCHAR(10) NOT NULL,
+  `country_code` VARCHAR(2) NULL,
+  `phone` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sub_category`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sub_category` ;
+
+CREATE TABLE IF NOT EXISTS `sub_category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `main_category`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `main_category` ;
+
+CREATE TABLE IF NOT EXISTS `main_category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(100) NOT NULL,
+  `last_name` VARCHAR(100) NOT NULL,
+  `address_id` INT NOT NULL,
+  `email` VARCHAR(100) NULL,
+  `reputation` DOUBLE NULL,
+  `create_date` DATETIME NULL,
+  `last_update` DATETIME NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(200) NOT NULL,
+  `enabled` TINYINT NOT NULL,
+  `role` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `username_UNIQUE` ON `user` (`username` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `rating` ;
+
+CREATE TABLE IF NOT EXISTS `rating` (
+  `id` INT NOT NULL,
+  `rating` DOUBLE NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS beerbuds@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'beerbuds'@'localhost' IDENTIFIED BY 'password';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'beerbuds'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
