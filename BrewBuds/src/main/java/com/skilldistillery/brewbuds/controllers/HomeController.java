f3149dbe03d4a2a837dff74a0fddb95a91aa25f2
Matthew Lee <matthewlee686@gmail.com>
2021-07-20 17:09:56 -0400

    Updated Home Page and added Rating to Table

--- BrewBuds/src/main/java/com/skilldistillery/brewbuds/controllers/HomeController.java
+++ BrewBuds/src/main/java/com/skilldistillery/brewbuds/controllers/HomeController.java
@@ -1,16 +1,22 @@
 package com.skilldistillery.brewbuds.controllers;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skilldistillery.brewbuds.data.BeerDAO;
 import com.skilldistillery.brewbuds.data.BreweryDAO;
 import com.skilldistillery.brewbuds.data.RatingDAO;
 import com.skilldistillery.brewbuds.data.UserDAO;
+import com.skilldistillery.brewbuds.entities.Beer;
 
 @Controller
 public class HomeController {
@@ -29,10 +35,22 @@ public class HomeController {
 	
 	@RequestMapping(path = {"/", "home.do"}, method = RequestMethod.GET)
 	public String home(Model model) {
-		model.addAttribute("beers", beerDAO.showTopFive());
-		model.addAttribute("breweries", breweryDAO.showTopFive());
-		Double average = ratingDao.findAverageBeerRating(id);
-		model.addAttribute("average", average);
+//		model.addAttribute("beers", beerDAO.showTopFive());
+//		model.addAttribute("breweries", breweryDAO.showTopFive());
+//		Double average = ratingDao.findAverageBeerRating(id);
+//		model.addAttribute("average", average);
+		
+		Map<Beer, Double> beerAndRating = new HashMap<>(); 
+		List<Beer> beers = beerDAO.showTopFive();
+		
+		for(Beer beer : beers) {
+			Double beerRating = ratingDao.findAverageBeerRating(beer.getId()); 
+			beerAndRating.put(beer, beerRating); 
+		}
+		
+		model.addAttribute("beers", beerAndRating); 
+		
+		
 		return "home";
 	}
 	
