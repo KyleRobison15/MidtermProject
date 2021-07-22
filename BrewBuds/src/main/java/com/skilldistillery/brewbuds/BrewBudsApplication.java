90f0f3988c5883feaf583897165ec7a9b4cae317
Chris <cwriddle5@gmail.com>
2021-07-22 16:29:51 -0400

    added war file and gradle stuff to deploy on aws

--- BrewBuds/src/main/java/com/skilldistillery/brewbuds/BrewBudsApplication.java
+++ BrewBuds/src/main/java/com/skilldistillery/brewbuds/BrewBudsApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.brewbuds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BrewBudsApplication {
+public class BrewBudsApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BrewBudsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrewBudsApplication.class, args);
